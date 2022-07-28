1a656843a348e03f35f1aba7120e2d5256e121dd
DanielMikitiuk <86248084+DanielMikitiuk@users.noreply.github.com>
2022-07-28 16:51:23 +0200

    hw4_gallery_php
    ))))

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
 <body>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/project-master">Hw4</a>
+      <a class="navbar-brand" href="/hw4_gallery_php">Hw4</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/project-master">Home</a>
+                <a class="nav-link active" aria-current="page" href="/hw4_gallery_php">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="index.php?page=contacts">Contacts</a>
